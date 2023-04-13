db1c43541e7839d8fb3155c532891c8dcf63da83
LazaroDeTormes <paladinalex470@gmail.com>
2023-04-13 13:05:54 +0200

    botones

--- app/src/main/java/com/example/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.proyectomoviles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView imgTitulo;
+    private LinearLayout pantallaPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgTitulo = findViewById(R.id.imgTitulo);
+        pantallaPrincipal = findViewById(R.id.pantallaTitulo);
+
+        imgTitulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pantallaPrincipal.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
