226a701f76e820ef818fb8a489223665558a4af2
Alejandro Vázquez González <105195259+LazaroDeTormes@users.noreply.github.com>
2023-04-17 15:37:12 +0200

    a

--- app/src/main/java/com/example/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.proyectomoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     private ImageView imgTitulo;
     private LinearLayout pantallaPrincipal;
+    private RelativeLayout pantBot;
+
+    private Button btnGen, btnAnho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,46 @@ public class MainActivity extends AppCompatActivity {
 
         imgTitulo = findViewById(R.id.imgTitulo);
         pantallaPrincipal = findViewById(R.id.pantallaTitulo);
+        pantBot = findViewById(R.id.pantallaBotones);
+        btnGen = findViewById(R.id.btnGeneros);
+        btnAnho = findViewById(R.id.btnAños);
+
 
         imgTitulo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 pantallaPrincipal.setVisibility(View.GONE);
+                pantBot.setVisibility(View.VISIBLE);
             }
         });
 
+        btnGen.setOnClickListener(this);
+        btnAnho.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Class clase;
+
+        switch(v.getId()){
+            case R.id.btnAños:
+                clase = pantallaAnhos.class;
+                cargarPantalla(clase);
+                break;
+            case R.id.btnGeneros:
+                clase = pantallaGeneros.class;
+                cargarPantalla(clase);
+                break;
+        }
+
+    }
+
+    public void cargarPantalla(Class clase){
+        Intent i = new Intent(this, clase);
+        startActivity(i);
     }
 }
