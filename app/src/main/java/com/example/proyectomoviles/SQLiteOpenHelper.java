0691c243f613c03d53f4fd8abe409e037d3f391c
LazaroDeTormes <paladinalex470@gmail.com>
2023-04-18 11:35:48 +0200

    botones

--- app/src/main/java/com/example/proyectomoviles/SQLiteOpenHelper.java
+++ app/src/main/java/com/example/proyectomoviles/SQLiteOpenHelper.java
@@ -26,7 +26,7 @@ public class SQLiteOpenHelper extends android.database.sqlite.SQLiteOpenHelper{
 
         try{
             db.execSQL("INSERT INTO canciones (codigo, nombre, grupo, duracion, genero, decada) VALUES (1, 'Bad Company', 'Five Finger Death Punch', '4:22', 'Metal', 2010)");
-            db.execSQL("INSERT INTO canciones (codigo, nombre, grupo, duracion, genero, decada) VALUES (2, 'Bad Company', 'Five Finger Death Punch', '4:22', 'Metal', 2010)");
+            db.execSQL("INSERT INTO canciones (codigo, nombre, grupo, duracion, genero, decada) VALUES (2, 'Far From Home', 'Five Finger Death Punch', '3:32', 'Metal', 2010)");
         } catch (Exception e){
             Toast.makeText(context, "ERROR DE INSERCION", Toast.LENGTH_SHORT).show();
         }
